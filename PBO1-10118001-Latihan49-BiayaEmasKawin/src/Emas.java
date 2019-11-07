6109cc9b42c77805b834a4585410b119ae018c87
Alfian Soasiu <55717248+AlfianSoasiu@users.noreply.github.com>
2019-11-08 00:00:09 +0700

    Add files via upload

---
+++ PBO1-10118001-Latihan49-BiayaEmasKawin/src/Emas.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama      : Alfian Soasiu
+ * Kelas     : PBO1
+ * NIM       : 10118001
+ * Deskripsi : Proses biaya emas kawin
+ */
+public class Emas {
+    
+   public int hargaGram;
+   public double beratEmas;
+
+    public int getHargaGram() {
+        return hargaGram;
+    }
+
+    public void setHargaGram(int hargaGram) {
+        this.hargaGram = hargaGram;
+    }
+
+    public double getBeratEmas() {
+        return beratEmas;
+    }
+
+    public void setBeratEmas(double beratEmas) {
+        this.beratEmas = beratEmas;
+    }
+   
+    public double perhitungan(int hargaGram, double beratEmas) {
+       
+       return hargaGram * beratEmas ;
+    }
+    
+}
